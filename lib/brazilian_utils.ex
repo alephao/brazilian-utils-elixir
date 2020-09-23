07ffa3758e1a01283e6b6d34337072a4a9e6584c
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-09-23 12:52:09 -0300

    feat: add functions to BrazilianUtils module

--- lib/brazilian_utils.ex
+++ lib/brazilian_utils.ex
@@ -1,2 +1,21 @@
 defmodule BrazilianUtils do
+  @spec get_cities(String.t()) :: [String.t()]
+  def get_cities(uf) when is_binary(uf) do
+    BrazilianUtils.DataSource.get_cities(uf)
+  end
+
+  @spec get_states() :: [%{code: String.t(), name: String.t()}]
+  def get_states() do
+    BrazilianUtils.DataSource.get_states()
+  end
+
+  @spec is_cep_valid?(String.t() | integer()) :: boolean()
+  def is_cep_valid?(cep) when is_binary(cep) or is_integer(cep) do
+    BrazilianUtils.Cep.is_valid?(cep)
+  end
+
+  @spec is_cpf_valid?(String.t()) :: boolean()
+  def is_cpf_valid?(cpf) when is_binary(cpf) do
+    BrazilianUtils.Cpf.is_valid?(cpf)
+  end
 end
