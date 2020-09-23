163e62ba9c59d80dbd2b1065d35c18dd6f498aeb
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-09-23 17:00:08 -0300

    docs: add docs to get_states

--- lib/brazilian_utils.ex
+++ lib/brazilian_utils.ex
@@ -19,6 +19,42 @@ defmodule BrazilianUtils do
     BrazilianUtils.DataSource.get_cities(uf)
   end
 
+  @doc ~S"""
+  Get a list with the name and UF of all Brazilian states.
+
+  ## Examples
+
+    iex> BrazilianUtils.get_states()
+    [
+      %{code: "AC", name: "Acre"},
+      %{code: "AL", name: "Alagoas"},
+      %{code: "AM", name: "Amazonas"},
+      %{code: "AP", name: "Amapá"},
+      %{code: "BA", name: "Bahia"},
+      %{code: "CE", name: "Ceará"},
+      %{code: "DF", name: "Distrito Federal"},
+      %{code: "ES", name: "Espírito Santo"},
+      %{code: "GO", name: "Goiás"},
+      %{code: "MA", name: "Maranhão"},
+      %{code: "MG", name: "Minas Gerais"},
+      %{code: "MS", name: "Mato Grosso do Sul"},
+      %{code: "MT", name: "Mato Grosso"},
+      %{code: "PA", name: "Pará"},
+      %{code: "PB", name: "Paraíba"},
+      %{code: "PE", name: "Pernambuco"},
+      %{code: "PI", name: "Piauí"},
+      %{code: "PR", name: "Paraná"},
+      %{code: "RJ", name: "Rio de Janeiro"},
+      %{code: "RN", name: "Rio Grande do Norte"},
+      %{code: "RO", name: "Rondônia"},
+      %{code: "RR", name: "Roraima"},
+      %{code: "RS", name: "Rio Grande do Sul"},
+      %{code: "SC", name: "Santa Catarina"},
+      %{code: "SE", name: "Sergipe"},
+      %{code: "SP", name: "São Paulo"},
+      %{code: "TO", name: "Tocantins"}
+    ]
+  """
   @spec get_states() :: [%{code: String.t(), name: String.t()}]
   def get_states() do
     BrazilianUtils.DataSource.get_states()
