cb1659740ec60bc4cfab3e51a14fbaa52bf56d90
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-09-23 17:54:07 -0300

    build: update mix.exs with docs metadata

--- mix.exs
+++ mix.exs
@@ -5,8 +5,36 @@ defmodule BrazilianUtils.MixProject do
     [
       app: :brazilian_utils,
       version: "0.1.0",
+      description: "Utilities library for Brazilian Businesses",
       elixir: "~> 1.10",
-      start_permanent: Mix.env() == :prod
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package()
+    ] ++ docs()
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package() do
+    [
+      maintainers: ["Aleph Retamal"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/alephao/brazilian-utils-elixir"}
+    ]
+  end
+
+  defp docs() do
+    [
+      name: "BrazilianUtils",
+      source_url: "https://github.com/alephao/brazilian-utils-elixir",
+      docs: [
+        main: "BrazilianUtils", # The main page in the docs
+        extras: ["README.md"]
+      ]
     ]
   end
 end
