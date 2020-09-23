0e362f4f6236e1af55d1877bf2cc7b78dcefacbe
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-09-23 12:47:41 -0300

    refact: add question mark to Cep.is_valid

--- lib/brazilian_utils/cep.ex
+++ lib/brazilian_utils/cep.ex
@@ -1,22 +1,22 @@
 defmodule BrazilianUtils.Cep do
   alias BrazilianUtils.Helper
 
-  @spec is_valid(cep :: String.t() | integer()) :: boolean()
-  def is_valid(cep)
+  @spec is_valid?(cep :: String.t() | integer()) :: boolean()
+  def is_valid?(cep)
 
-  def is_valid(cep) when is_binary(cep) do
+  def is_valid?(cep) when is_binary(cep) do
     cep
     |> Helper.only_numbers()
-    |> is_valid_cep_length
+    |> is_valid_cep_length?
   end
 
-  def is_valid(cep) when is_integer(cep) do
+  def is_valid?(cep) when is_integer(cep) do
     Integer.to_string(cep)
-    |> is_valid
+    |> is_valid?
   end
 
-  @spec is_valid_cep_length(cep :: String.t()) :: boolean()
-  defp is_valid_cep_length(cep) when is_binary(cep) do
+  @spec is_valid_cep_length?(cep :: String.t()) :: boolean()
+  defp is_valid_cep_length?(cep) when is_binary(cep) do
     String.length(cep) == 8
   end
 end
--- test/brazilian_utils/cep_test.exs
+++ test/brazilian_utils/cep_test.exs
@@ -4,18 +4,18 @@ defmodule BrazilianUtils.CepTests do
   alias BrazilianUtils.Cep
 
   test "is_valid" do
-    assert Cep.is_valid("12345678") == true
-    assert Cep.is_valid("12345-678") == true
-    assert Cep.is_valid(12_345_678) == true
+    assert Cep.is_valid?("12345678") == true
+    assert Cep.is_valid?("12345-678") == true
+    assert Cep.is_valid?(12_345_678) == true
 
-    assert Cep.is_valid("1") == false
-    assert Cep.is_valid("123456789") == false
-    assert Cep.is_valid("12345-6789") == false
-    assert Cep.is_valid(0) == false
-    assert Cep.is_valid(123_456_789) == false
+    assert Cep.is_valid?("1") == false
+    assert Cep.is_valid?("123456789") == false
+    assert Cep.is_valid?("12345-6789") == false
+    assert Cep.is_valid?(0) == false
+    assert Cep.is_valid?(123_456_789) == false
 
     assert_raise FunctionClauseError, fn ->
-      Cep.is_valid(%{foo: :bar})
+      Cep.is_valid?(%{foo: :bar})
     end
   end
 end
