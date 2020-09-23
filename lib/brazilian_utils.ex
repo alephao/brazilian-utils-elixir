53fe93e0230297a01d3f3e51fa8aef7e4fc11935
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2020-09-23 16:36:44 -0300

    refact: fix names is_x_valid? -> is_valid_x?

--- lib/brazilian_utils.ex
+++ lib/brazilian_utils.ex
@@ -9,28 +9,28 @@ defmodule BrazilianUtils do
     BrazilianUtils.DataSource.get_states()
   end
 
-  @spec is_cep_valid?(String.t() | integer()) :: boolean()
-  def is_cep_valid?(cep) when is_binary(cep) or is_integer(cep) do
+  @spec is_valid_cep?(String.t() | integer()) :: boolean()
+  def is_valid_cep?(cep) when is_binary(cep) or is_integer(cep) do
     BrazilianUtils.Cep.is_valid?(cep)
   end
 
-  @spec is_cpf_valid?(String.t()) :: boolean()
-  def is_cpf_valid?(cpf) when is_binary(cpf) do
+  @spec is_valid_cpf?(String.t()) :: boolean()
+  def is_valid_cpf?(cpf) when is_binary(cpf) do
     BrazilianUtils.Cpf.is_valid?(cpf)
   end
 
-  @spec is_phone_valid?(String.t()) :: boolean()
-  def is_phone_valid?(phone) when is_binary(phone) do
+  @spec is_valid_phone?(String.t()) :: boolean()
+  def is_valid_phone?(phone) when is_binary(phone) do
     BrazilianUtils.Phone.is_valid?(phone)
   end
 
-  @spec is_mobile_valid?(String.t()) :: boolean()
-  def is_mobile_valid?(phone) when is_binary(phone) do
+  @spec is_valid_mobile?(String.t()) :: boolean()
+  def is_valid_mobile?(phone) when is_binary(phone) do
     BrazilianUtils.Phone.is_valid_mobile_phone?(phone)
   end
 
-  @spec is_landline_valid?(String.t()) :: boolean()
-  def is_landline_valid?(phone) when is_binary(phone) do
+  @spec is_valid_landline?(String.t()) :: boolean()
+  def is_valid_landline?(phone) when is_binary(phone) do
     BrazilianUtils.Phone.is_valid_landline_phone?(phone)
   end
 end
